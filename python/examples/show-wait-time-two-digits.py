1cbc46fecedc9e171d5755921b717268a57d3e04
Leon Steinert <steinert@amicaldo.de>
2025-12-18 23:29:10 +0100

    Add Python example scripts for e-paper displays, fonts, and assets

---
+++ python/examples/show-wait-time-two-digits.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in9_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd2in9 V2 Demo") 
+    epd = epd2in9_V2.EPD()
+
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+    
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+    font35 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 35)
+    font60 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 60)
+
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Himage)
+    draw.text((110, 20), sys.argv[1], font = font60, fill = 0)
+    draw.text((108, 80), 'Minuten', font = font24, fill = 0)
+    # Himage = Himage.transpose(method=Image.ROTATE_180)
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
+    epd.sleep()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in9_V2.epdconfig.module_exit(cleanup=True)
+    exit()
